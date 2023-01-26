da3e549a4cda63cc40f7b0ecf6869bb218b596bd
duhanmeric <duhanmeric@gmail.com>
2023-01-26 22:19:35 +0300

    v0.0.2

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-// version number = 0.0.1
+// version number = 0.0.2
 
 const toggleItem = document.querySelectorAll(".toggle");
 
-const toggleFn = (item, interval) => {
+const toggleFn = (item, interval, text) => {
   console.log(interval);
   let isShow = true;
 
   setInterval(function () {
     isShow = !isShow;
     item.style.display = isShow ? "block" : "none";
+    item.innerHTML = text;
   }, interval);
 };
 
 toggleItem.forEach((item) => {
   const dataInterval = parseInt(item.getAttribute("data-interval"));
+  const dataText = item.getAttribute("data-text");
   if (dataInterval && dataInterval > 0) {
-    toggleFn(item, dataInterval);
+    toggleFn(item, dataInterval, dataText);
   }
 });
